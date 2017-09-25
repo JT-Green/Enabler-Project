71c1c7198c3ae9a71f2921d35c2911eecf72c101
JT-Green <31543546+JT-Green@users.noreply.github.com>
2017-09-25 00:04:33 -0400

    In-progress reworking to allow multiline searches

--- charon_v0-9.py
+++ charon_v0-9.py
@@ -5,11 +5,9 @@ import tkinter as tk
 from tkinter import messagebox
 import win32com.client
 import csv
-from PIL import ImageGrab
 
 VP_JSON = r'values_pairs.json'
-VP_CSV = r'H:Sales\Public\National Accounts\National Accounts\jt\value_pairs.csv'
-
+VP_CSV = r'C:\Users\JT\Desktop\test_csv.csv'
 
 def create_lookup():
     """
@@ -23,63 +21,10 @@ def create_lookup():
         json.dump(values_pairs, f_obj)
 
 
-def match_pairs():
-    """Loads local json file into a dict, and matches based on input."""
-
-    with open(VP_JSON) as f_obj:
-        values_pairs = json.load(f_obj)
-
-    output_list = []
-    errors = []
-    for value in lookup:
-        clean_lookup = value.strip().replace("-", "")
-
-        # Looks for corresponding Product ID of the ISBN.
-        if len(clean_lookup) == 13 and clean_lookup.startswith("978"):
-            if clean_lookup in values_pairs:
-                output_list.append(values_pairs[clean_lookup])
-            else:
-                errors.append(clean_lookup)
-                output_list.append("Not found")
-
-        # Looks for corresponding ISBN of the Product ID.
-        else:
-            if clean_lookup in values_pairs.keys():
-                isbn = list(values_pairs.keys())[
-                        list(values_pairs.values()).index(clean_lookup)]
-                output_list.append(isbn)
-            else:
-                errors.append(clean_lookup)
-                output_list.append("Not found")
-
-    if len(errors):
-        error_msg = '\n'.join(errors)
-
-        if len(errors) == 1:
-            msg = "A valid value for {} could not be found.".format(error_msg)
-        else:
-            msg = ("Valid values for the following could not be found: \n\n"
-                   .format(error_msg)
-                   )
-        hide_root()
-        messagebox.showinfo("No Match Found", msg)
-
-    
-
-
-
-def show_unmatched():
-    """Informs user of no match, and allows them to see what they input."""
-
-    # Limits displayed input to 30 characters for readability.
-    if len(lookup) > 30:
-        lookup_error = "{} . . . ".format(lookup[:30])
-    else:
-        lookup_error = lookup
-
-    no_match_msg = "There was no valid match for:\n\n {}".format(lookup_error)
-    hide_root()
-    messagebox.showinfo("No Match Found", no_match_msg)
+def hide_root():
+    """Just hides the Tkinter terminal window so it doesn't pop up."""
+    root = tk.Tk()
+    root.withdraw()
 
 
 def update_available():
@@ -96,79 +41,77 @@ def should_update_json():
     hide_root()
 
     if messagebox.askyesno("Update?",
-                "There was no valid match."
-                "\nThere is an update available for the values list."
-                "\nWould you like to update and try again?"
-                "\n(If you update and do not see an error, a match was found.)"
+                           "There was no valid match."
+                           "\nThere is an update available for the values list."
+                           "\nWould you like to update and try again?"
+                           "\n(If you update and do not see an error, a match was found.)"
                            ):
         return True
 
+def show_unmatched():
+    """Informs user of no match, and allows them to see what they input."""
 
-def hide_root():
-    """Just hides the Tkinter terminal window so it doesn't pop up."""
-    root = tk.Tk()
-    root.withdraw()
+    error_msg = '\n'.join(errors)
 
+    if len(errors) == 1:
+        msg = "A valid value for {} could not be found.".format(error_msg)
 
-def email_unexpected_error():
-    """Tells user an unexpected error occurred, and encourages emailing JT."""
+    else:
+        msg = ("Valid values for the following could not be found: \n\n"
+               .format(error_msg)
+               )
 
     hide_root()
+    messagebox.showinfo("No Match Found", msg)
 
-    error_msg = ("There was an unexpected error. Would you like to email JT?\n" 
-                 "Doing so will help make improvements so that this doesn't "
-                 "happen again.")
-
-    if messagebox.askyesno("Unexpected Enabler Error", error_msg):
-
-        o = win32com.client.Dispatch("Outlook.Application")
-
-        msg = o.CreateItem(0)
-        msg.Subject = "Enabler Error Submission"
-        msg.HTMLBody = (
-            "Hi JT,<br><br>I encountered an error while using Enabler. "
-            "Here is what I was doing when the error occurred:<br><br>"
-            "<b>What I was trying to convert: </b> {}"
-            "<br><b>More details about what I was doing: (As detailed as "
-            "possible, please)</b> "
-            "<br><br>Eternally grateful,<br>".format(lookup))
-
-        msg.To = "jt@workman.com"
-
-        msg.Display()
-
-
-###############################################################################
 lookup = pyperclip.paste().splitlines()
+output_list = []
+errors = []
 
 while True:
 
-    # if not ImageGrab.grabclipboard():
-    #
-    #
-    # else:
-    #     hide_root()
-    #     messagebox.showinfo("Charon Error", "Your clipboard contains an image. "
-    #                                     "Please search for text only.")
-    #     break
-
     try:
-        match_pairs()
-        break
-
+        with open(VP_JSON) as f_obj:
+            values_pairs = json.load(f_obj)
     except FileNotFoundError:
         create_lookup()
+        continue
 
-    except (KeyError, ValueError):
+    for value in lookup:
+        clean_lookup = value.strip().replace("-", "")
+
+        # Looks for corresponding Product ID of the ISBN.
+        if len(clean_lookup) == 13 and clean_lookup.startswith("978"):
+            if clean_lookup in values_pairs:
+                output_list.append(values_pairs[clean_lookup])
+            else:
+                errors.append(clean_lookup)
+                output_list.append("Not found")
+
+        # Looks for corresponding ISBN of the Product ID.
+        else:
+            if clean_lookup in values_pairs.values():
+                isbn = list(values_pairs.keys())[list(values_pairs.values()).index(clean_lookup)]
+                output_list.append(isbn)
+            else:
+                errors.append(clean_lookup)
+                output_list.append("Not found")
+
+    if len(errors):
         if update_available():
             if should_update_json():
                 create_lookup()
-            else:
-                break
-        else:
-            show_unmatched()
-            break
+                continue
 
-    #except Exception:
-        #email_unexpected_error()
-        #break
+        show_unmatched()
+
+    copy_output = "\n".join(output_list)
+    pyperclip.copy(copy_output)
+    break
+
+print(len(output_list))
+
+if not output_list: print("nah")
+
+# If clean_lookup is blank, return blank
+# if NOT output_list, error message
